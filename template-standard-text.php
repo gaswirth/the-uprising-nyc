90419c6e885137bb1680d293189b9d0c224ac437
Nick Gaswirth <nick@roundhouse-designs.com>
2016-03-31 16:14:37 -0400

    new pages and styles added.

---
+++ template-standard-text.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Standard Text Pages
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+			<div class="page-inner">
+				<?php if ( have_posts() ) : ?>
+
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'content', 'page-with-title' ); ?>
+
+					<?php endwhile; ?>
+
+				<?php endif; ?>
+			</div>
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php get_footer(); ?>
